2bbf6bf0cb4f0cd3325dba22b27a1c9ff52f21d4
Alex Grygorenko <alexey.grygorenko@gmail.com>
2019-09-15 16:35:04 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Component1 from './components/Component1';
+import Component2 from './components/Component2';
+import NotFound from './components/NotFound';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <NavBar></NavBar>
+        <Switch>
+          <Route exact path="/" component={Component1}></Route>
+          <Route exact path="/component2" component={Component2}></Route>
+          <Route component={NotFound}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Component2.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Component2 = () => {
+  return <div>I`m second component!</div>;
+};
+
+export default Component2;
